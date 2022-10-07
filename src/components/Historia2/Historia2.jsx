319dc2496ec7535a0253964762dc78a76b9f67f3
Franchesca <franchesca1905@hotmail.com>
2022-10-07 11:50:36 -0500

    terminando historia 2

--- src/components/Historia2/Historia2.jsx
+++ src/components/Historia2/Historia2.jsx
@@ -1,9 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import {Link} from "react-router-dom";
 import "./Historia2.css";
 
 const Historia2 =() =>{
 
     return(
-        <h1>HOLA</h1>
+        <Container className="Login">
+        <Row>
+           <div className="create_login">
+                <div className="title_login">
+                    <p>LOG IN.</p>
+                </div>
+                <div className="text-help1">
+                    <small id="text">Log in with your details below to view your order.</small>
+                </div>
+                <div className= "inputs">
+                    <input type="email" class="form-control" id="login" placeholder="Email" />
+                    <input type="password" class="form-control" id="login" placeholder="Password" />
+                </div>
+                <Link to="/"><button type="submit" id="create_boton" aria-describedby="forgotPassword">LOGIN</button></Link>
+                <div className="text-help1">
+                    <small id="text">Forgot your password?</small>
+                </div>
+                <div className="text-help2">
+                    <small id="noAccount"><strong>Don't have an account?</strong></small>
+                </div>
+                <Link to="/historia2"><button type="submit" id="create_boton">SIGN UP</button></Link>
+           </div>
+           
+        </Row>
+    </Container>
     )
 
 }
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -8,7 +8,8 @@ const LoginPage = () => {
             main={<Login />}>
                 
             </Layout>
-        </>)
+        </>
+    )
 
 }
 

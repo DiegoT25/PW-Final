76436eb3436993ab5985a6631e1eb09c0b7ac958
Franchesca <franchesca1905@hotmail.com>
2022-10-09 01:38:56 -0500

    comenzando historia5

--- src/components/Historia5/Historia5.jsx
+++ src/components/Historia5/Historia5.jsx
@@ -1,8 +1,17 @@
+import { Container } from "react-bootstrap";
+import { Col } from "react-bootstrap"
 import "./Historia5.css";
 
 const Historia5 =() =>{
     return(
-        <h1>HOLA</h1>
+
+        <Container className="profile">
+            <Col>
+                <div className="data_profile">
+                    
+                </div>
+            </Col>
+        </Container>
     )
 }
 

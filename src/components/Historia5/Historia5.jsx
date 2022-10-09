a155f12452dd9cc888a5c5b7ceb722da282ac00b
Franchesca <franchesca1905@hotmail.com>
2022-10-09 09:56:10 -0500

    realizando historia5

--- src/components/Historia5/Historia5.jsx
+++ src/components/Historia5/Historia5.jsx
@@ -1,5 +1,5 @@
 import { Container } from "react-bootstrap";
-import { Col } from "react-bootstrap"
+import { Col } from "react-bootstrap";
 import "./Historia5.css";
 
 const Historia5 =() =>{
@@ -8,7 +8,18 @@ const Historia5 =() =>{
         <Container className="profile">
             <Col>
                 <div className="data_profile">
-                    
+                    <div className="columna1">
+                        <h10><strong>Default</strong></h10>
+                        <div className="p1">
+                            <p>Ed Va</p>
+                        </div>
+                        <div className="p2">
+                            <p>United States</p>    
+                        </div>
+                    </div>
+                    <div className="columna2">
+                        <h1>Hola</h1>
+                    </div>
                 </div>
             </Col>
         </Container>

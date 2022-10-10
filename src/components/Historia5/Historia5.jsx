9bed96ea0937964102d3b46397f24b30b55336fe
Franchesca <franchesca1905@hotmail.com>
2022-10-10 09:31:04 -0500

    terminando historia 5 con botones

--- src/components/Historia5/Historia5.jsx
+++ src/components/Historia5/Historia5.jsx
@@ -1,5 +1,7 @@
-import { Container } from "react-bootstrap";
-import { Col } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
 import "./Historia5.css";
 
 const Historia5 =() =>{
@@ -33,27 +35,42 @@ const Historia5 =() =>{
                             <div className="simple">
                                 <label>Email</label>
                                 <input type="email" class="form-control" id="form_profile1" placeholder="" />
-                                <label>Address</label>
-                                <input type="text" class="form-control" id="form_profile1" placeholder="" />
+                                <div className="address">
+                                    <label>Address</label>
+                                    <input type="text" class="form-control" id="form_profile1" placeholder="" />
+                                </div>
                             </div>
                             <div className="doble">
-                                <label>Aparmaent, suit, etc</label>
-                                <input type="text" class="form-control" id="form_profile2" placeholder="" />
-                                <label>City</label>
-                                <input type="text" class="form-control" id="form_profile2" placeholder="" />
+                                <div className="label3">
+                                    <label>Aparmaent, suit, etc</label>
+                                    <input type="text" class="form-control" id="form_profile2" placeholder="" />
+                                </div>
+                                <div className="label4">
+                                    <label>City</label>
+                                    <input type="text" class="form-control" id="form_profile2" placeholder="" />
+                                </div>
                             </div>
                             <div className="doble">
-                                <label>Country/Region</label>
-                                <select class="form-control" id="form_profile2" placeholder="" />
-                                <label>Postal/zip code</label>
-                                <input type="text" class="form-control" id="form_profile2" placeholder="" />
+                                <div className="label1">
+                                    <label>Country/Region</label>
+                                    <select class="form-control" id="form_profile2" placeholder="" />
+                                </div>
+                                <div className="label2">
+                                    <label>Postal/zip code</label>
+                                    <input type="text" class="form-control" id="form_profile2" placeholder="" />
+                                </div>
                             </div>
                             <div className="simple">
                                 <label>Phone</label>
                                 <input type="text" class="form-control" id="form_profile1" placeholder="" />
                             </div>
                         </div>
-                        
+                        <div className="update_info">
+                            <Nav.Link href=""><Button type="submit" id="update_button">Update Info</Button></Nav.Link>
+                        </div>
+                        <div className="cancel_button">
+                            <Nav.Link href=""><Button type="reset" id="cancel_button">Cancel</Button></Nav.Link>
+                        </div>
                     </div>
                 </div>
             </Col>

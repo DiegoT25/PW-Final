2f6c103ebd1c81ce5f2d9b1688caf6b5cb575015
Diego <diegotello2000@hotmail.com>
2022-10-13 11:27:10 -0500

    Modificando historia 14

--- src/components/Historia14/Historia14.jsx
+++ src/components/Historia14/Historia14.jsx
@@ -1,68 +1,59 @@
 import { Container } from "react-bootstrap";
 import {Col} from "react-bootstrap";
 import {Row} from "react-bootstrap";
-import { Nav } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-
 import './Historia14.css';
 const Historia14 = () => {
     return (
         <Container fluid className="contenedor">
             <Col fluid>
-                <Col fluid className="encabezados">
-                    <Row fluid className="encabezadol">
-                        <h1>Shopping car items</h1>
-                    </Row>
-                    <Row fluid className="encabezador">
-                        <Nav.Link href=""><Button id="Botoni">Checkout</Button></Nav.Link>
-                    </Row>
-                </Col>  
+         
                 <Col Fluid className="productos">
                     <Col fluid className="lista">
                         
                         <Col fluid className="foto2"> foto </Col>
                         <Col fluid className="texto"><h4>Keyboard & mouse bundle</h4> </Col>
                         <Col fluid className="precio"> <h4>$39</h4></Col>
-                        
+                        <Col fluid className="fecha">10/12/2022</Col>
                     </Col>
                 </Col>
-                <Col fluid className="productos2">
+                <Col fluid className="productos">
                     <Col fluid className="lista">
                         <Col fluid className="foto2"> foto </Col>
                         <Col fluid className="texto"><h4>INTEL CORE I7 - 12700F 12-CORE</h4> </Col>
                         <Col fluid className="precio"> <h4>$359</h4></Col>
-                        
+                        <Col fluid className="fecha">10/12/2022</Col>
                     </Col>
                 </Col>
-                <Col fluid className="productos3">
+                <Col fluid className="productos">
                     <Col fluid className="lista">
                         <Col fluid className="foto2"> foto </Col>
-                        <Col fluid className="texto"><h4>NVIDI GEFORCE RTX 3070 8GB</h4> </Col>
+                        <Col fluid className="text"><h4>NVIDIA GEFORCE RTX 3070 8GB</h4> </Col>
                         <Col fluid className="precio"> <h4>$679</h4></Col>
-                        
+                        <Col fluid className="fecha">10/12/2022</Col>
                     </Col>
                 </Col>
-                <Col fluid className="productos4">
+                <Col fluid className="productos">
                     <Col fluid className="lista">
                         <Col fluid className="foto2"> foto </Col>
-                        <Col fluid className="texto"><h4>1TB NVME M.2</h4> </Col>
+                        <Col fluid className="text"><h4>1TB NVME M.2</h4> </Col>
                         <Col fluid className="precio"> <h4>$99</h4></Col>
-                        
+                        <Col fluid className="fecha">10/12/2022</Col>
                     </Col>
           </Col>
-                <Col fluid className="productos5">
+                <Col fluid className="productos">
                     <Col fluid className="lista">
                         <Col fluid className="foto2"> foto </Col>
-                        <Col fluid className="texto"><h4>COOLER MASTER TD500 RGB</h4> </Col>
+                        <Col fluid className="text"><h4>COOLER MASTER TD500 RGB</h4> </Col>
                         <Col fluid className="precio"> <h4>$99</h4></Col>
-                        
+                        <Col fluid className="fecha">10/12/2022</Col>
                     </Col>
                 </Col>
-                <Col fluid className="productos6">
+                <Col fluid className="productos">
                     <Col fluid className="lista">
                         <Col fluid className="foto2"> foto </Col>
-                        <Col fluid className="texto"><h4>BUILD + SETUP + TESTING</h4> </Col>
+                        <Col fluid className="text"><h4>BUILD + SETUP + TESTING</h4> </Col>
                         <Col fluid className="precio"> <h4>$99</h4></Col>
+                        <Col fluid className="fecha">10/12/2022</Col>
                         
                     </Col>
                 </Col>

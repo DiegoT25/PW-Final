383710c9df08ec03b2da786c87b72b3bc0d5fb0d
Giancarlo19Lopez <giancarlo19042002@hotmail.com>
2022-10-14 18:59:56 -0500

    cambiando la historia 7

--- src/components/Historia7/Historia7.jsx
+++ src/components/Historia7/Historia7.jsx
@@ -3,7 +3,6 @@ import Col from "react-bootstrap/Col";
 import Row from 'react-bootstrap/Row';
 import Image from "react-bootstrap/Image";
 import producto1 from "./images_producto/rtx.png";
-import producto2 from "./images_producto/nvidia.png";
 import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import "./Historia7.css";
@@ -18,11 +17,63 @@ const Historia7 =() => {
                             <Image src={producto1} id="producto1"/>
                         </Col>
                         <Col fluid className="botoncito">
-                        <Nav.Link href="/historia8"><Button id="Botone">Add to the car</Button></Nav.Link>
+                            <Nav.Link href="/historia8"><Button id="Botone">Add to the car</Button></Nav.Link>
                         </Col>
                     </Row>
                     <Row fluid className="right">
-                    <Image src={producto2} id="producto2"/>
+                        <Row>
+                            <h3><b>NVIDIA GEFORCE GTX 1650 4GB</b></h3>
+                            <h3 className="precio"><b>$229</b></h3>
+                            <h4><b className="mini">Shipping</b> calculated at checkout</h4>
+                        </Row>
+                        <Row>
+                        <table>
+                            <tr>
+                                <td>
+                                    Chipset
+                                    manufacturer
+                                </td>
+                                <td>
+                                    NVIDIA
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    GPU
+                                </td>
+                                <td>
+                                    GeForce GTX 1650
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    CORE CLOCK
+                                </td>
+                                <td>
+                                    1530 MHz
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    BOOST CLOCK
+                                </td>
+                                <td>
+                                    OC Mode: 1800 Mhz
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    CUDA CORES
+                                </td>
+                                <td>
+                                    1280
+                                </td>
+                            </tr>
+                        </table>
+                        </Row>
+                    </Row>
+                    <Row className="izquierdon">
+
                     </Row>
                 </Col>
             </Row>

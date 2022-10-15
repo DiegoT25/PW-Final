53c74da7b38c566b54793f7e97c56bcc81774adf
Diego <diegotello2000@hotmail.com>
2022-10-15 04:29:49 -0500

    Agregando Historia22

---
+++ src/components/pages/Historia22Page.jsx
@@ -0,0 +1,15 @@
+import Layout1 from '../layout/Layout1';
+import Historia22 from '../Historia22';
+
+const Historia22Page =() =>{
+    return(
+        <>
+        <Layout1
+            main={<Historia22 />}>
+                
+            </Layout1>
+        </>
+    )
+}
+
+export default Historia22Page;

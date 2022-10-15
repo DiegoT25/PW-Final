e192d70df344e09d3fcd6604d29651b23e2b444f
Franchesca <franchesca1905@hotmail.com>
2022-10-14 20:32:14 -0500

    modificando h16

--- src/components/Historia16/Historia16.jsx
+++ src/components/Historia16/Historia16.jsx
@@ -14,9 +14,9 @@ const Historia16 =() =>{
         <Container className="Contenedor">
                 <Row className="fila">
                     <h1>Support Hub</h1>
-                    <Col><Nav.Link href="/Historia17"><Button id="boton4"><AiOutlineContainer /><br></br>GUIDES</Button></Nav.Link></Col>
-                    <Col><Nav.Link href="/Historia18"><Button id="boton4"><AiFillFileUnknown /><br></br>FAQ</Button></Nav.Link></Col>
-                    <Col><Nav.Link href="/Historia19"><Button id="boton4"><AiFillTag /><br></br>SUBMIT A TICKET</Button></Nav.Link></Col>
+                    <Col><Nav.Link href="/Historia17"><Button className="boton4" id="botonh16"><AiOutlineContainer /><br></br>GUIDES</Button></Nav.Link></Col>
+                    <Col><Nav.Link href="/Historia18"><Button className="boton4" id="botonh16"><AiFillFileUnknown /><br></br>FAQ</Button></Nav.Link></Col>
+                    <Col><Nav.Link href="/Historia19"><Button className="boton4" id="botonh16"><AiFillTag /><br></br>SUBMIT A TICKET</Button></Nav.Link></Col>
                 </Row>
         </Container>
     )

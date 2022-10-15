592622d77c59974879f88811f1a3088987976657
Diego <diegotello2000@hotmail.com>
2022-10-14 21:23:54 -0500

    Historia10 modificado

--- src/components/Historia10/Historia10.jsx
+++ src/components/Historia10/Historia10.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button'
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import './Historia10.css';
-import { BsController } from "react-icons/bs";
+import { BsController, BsBezier,BsCodeSquare, BsBadge4K, BsBuilding, BsShuffle } from "react-icons/bs";
 import { Nav } from "react-bootstrap";
 const Historia10 = () => {
     return (
@@ -11,14 +11,14 @@ const Historia10 = () => {
         
         <Row className="fila">
             <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Gaming</Button></Nav.Link></Col>
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Design</Button></Nav.Link></Col>
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Coding</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsBezier className="icono"/>Design</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsCodeSquare className="icono"/>Coding</Button></Nav.Link></Col>
         </Row>
 
         <Row className="fila">
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Rendering</Button></Nav.Link></Col>
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Office</Button></Nav.Link></Col>
-            <Col className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Other</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsBadge4K className="icono"/>Rendering</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsBuilding className="icono"/>Office</Button></Nav.Link></Col>
+            <Col className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsShuffle className="icono"/>Other</Button></Nav.Link></Col>
             </Row>
     </Container>
     )

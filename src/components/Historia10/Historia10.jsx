868c50192b3d3bd5e43880394d1d66ba2238aa56
Diego <diegotello2000@hotmail.com>
2022-10-14 22:49:09 -0500

    Modificando Historia10

--- src/components/Historia10/Historia10.jsx
+++ src/components/Historia10/Historia10.jsx
@@ -9,16 +9,16 @@ const Historia10 = () => {
     return (
     <Container fluid className="cont">
         
-        <Row className="fila">
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsController className="icono"/>Gaming</Button></Nav.Link></Col>
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsBezier className="icono"/>Design</Button></Nav.Link></Col>
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsCodeSquare className="icono"/>Coding</Button></Nav.Link></Col>
+        <Row className="fila10">
+            <Col  className="columnas"><Nav.Link className="boton" href="/historia11"><Button className="boton"><BsController className="icono" id="icono_10"/>Gaming</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/historia11"><Button className="boton"><BsBezier className="icono" id="icono_10"/>Design</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/historia11"><Button className="boton"><BsCodeSquare className="icono" id="icono_10"/>Coding</Button></Nav.Link></Col>
         </Row>
 
-        <Row className="fila">
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsBadge4K className="icono"/>Rendering</Button></Nav.Link></Col>
-            <Col  className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsBuilding className="icono"/>Office</Button></Nav.Link></Col>
-            <Col className="columnas"><Nav.Link className="boton" href="/gaming"><Button className="boton"><BsShuffle className="icono"/>Other</Button></Nav.Link></Col>
+        <Row className="fila10">
+            <Col  className="columnas"><Nav.Link className="boton" href="/historia11"><Button className="boton"><BsBadge4K className="icono" id="icono_10"/>Rendering</Button></Nav.Link></Col>
+            <Col  className="columnas"><Nav.Link className="boton" href="/historia11"><Button className="boton"><BsBuilding className="icono" id="icono_10"/>Office</Button></Nav.Link></Col>
+            <Col className="columnas"><Nav.Link className="boton" href="/historia11"><Button className="boton"><BsShuffle className="icono" id="icono_10"/>Other</Button></Nav.Link></Col>
             </Row>
     </Container>
     )

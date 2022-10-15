9cf9d117586f73d3847da31b0c3a671f5bbeebde
Franchesca <franchesca1905@hotmail.com>
2022-10-14 20:19:17 -0500

    modificando cambios

--- src/components/Historia16/Historia16.jsx
+++ src/components/Historia16/Historia16.jsx
@@ -12,10 +12,11 @@ import { AiFillFileUnknown } from "react-icons/ai";
 const Historia16 =() =>{
     return(
         <Container className="Contenedor">
-                <Row className="fila"><h1 className='h1'>Support Hub</h1>
-                    <Col><Nav.Link href="/Historia17"><Button className="boton4"><AiOutlineContainer /><br></br>GUIDES</Button></Nav.Link></Col>
-                    <Col><Nav.Link href="/Historia18"><Button className="boton4"><AiFillFileUnknown /><br></br>FAQ</Button></Nav.Link></Col>
-                    <Col><Nav.Link href="/Historia19"><Button className="boton4"><AiFillTag /><br></br>SUBMIT A TICKET</Button></Nav.Link></Col>
+                <Row className="fila">
+                    <h1>Support Hub</h1>
+                    <Col><Nav.Link href="/Historia17"><Button id="boton4"><AiOutlineContainer /><br></br>GUIDES</Button></Nav.Link></Col>
+                    <Col><Nav.Link href="/Historia18"><Button id="boton4"><AiFillFileUnknown /><br></br>FAQ</Button></Nav.Link></Col>
+                    <Col><Nav.Link href="/Historia19"><Button id="boton4"><AiFillTag /><br></br>SUBMIT A TICKET</Button></Nav.Link></Col>
                 </Row>
         </Container>
     )
